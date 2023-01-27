9a8c56f5b3256b82b4ae8ce2f126a934ab476729
BorisC <boris.condori@jalasoft.com>
2023-01-27 19:11:12 -0400

    Add context AuthProvider

--- HerosApp/src/HerosApp.jsx
+++ HerosApp/src/HerosApp.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import AppRouter from './router/AppRouter'
+import { AuthProvider } from './auth'
 
 const HerosApp = () => {
   return (
-    <AppRouter />
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
   )
 }
 
